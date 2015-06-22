a32fa3e32989d5e8e971b67eab29f49cb374d782
Thomas Reinholdsson <reinholdsson@gmail.com>
2015-06-22 23:53:21 +0200

    Add some error handling

--- R/rdigits.R
+++ R/rdigits.R
@@ -1,12 +1,22 @@
 #' Classify one image
 classify_one <- function(server_url, job_id, image_url) {
-  x <- POST(
+  res <- POST(
     sprintf('%s/models/images/classification/classify_one.json?job_id=%s', server_url, job_id),
     body = list(image_url = image_url)
   )
-  x <- content(x)
+  
+  x <- content(res)
+  
+  st_code <- status_code(res)
+  if (st_code != 200) {
+    warning(st_code, ', ', x$error$type, ': ', x$error$message, '\n => ', image_url)
+    return(NULL)
+  }
+  
   x <- do.call('rbind', lapply(x$predictions, function(i) data.frame(label = i[[1]], value = i[[2]])))
   x <- cbind(image_url, x)
+  #print(paste(ncol(x), image_url))
+  #browser()
   return(x)
 }
 
@@ -18,5 +28,10 @@ classify_one <- function(server_url, job_id, image_url) {
 #' @param ... arguments passed on to classify_one(...)
 #'
 #' @export
-classify <- function(images, ...)
-  do.call('rbind', pblapply(images, function(i) classify_one(image_url = i, ...)))
+classify <- function(images, ...) {
+  do.call('rbind', pblapply(images, function(i) {
+    x <- classify_one(image_url = i, ...)
+    #print(x)
+    return(x)
+  }))
+}
