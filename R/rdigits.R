48acc59a08831961fe8b5814ac19dbb7bc99fd71
Thomas Reinholdsson <reinholdsson@gmail.com>
2015-06-23 09:42:53 +0200

    Clean up

--- R/rdigits.R
+++ R/rdigits.R
@@ -4,19 +4,17 @@ classify_one <- function(server_url, job_id, image_url) {
     sprintf('%s/models/images/classification/classify_one.json?job_id=%s', server_url, job_id),
     body = list(image_url = image_url)
   )
-  
+
   x <- content(res)
-  
+
   st_code <- status_code(res)
   if (st_code != 200) {
     warning(st_code, ', ', x$error$type, ': ', x$error$message, '\n => ', image_url)
     return(NULL)
   }
-  
+
   x <- do.call('rbind', lapply(x$predictions, function(i) data.frame(label = i[[1]], value = i[[2]])))
   x <- cbind(image_url, x)
-  #print(paste(ncol(x), image_url))
-  #browser()
   return(x)
 }
 
@@ -31,7 +29,6 @@ classify_one <- function(server_url, job_id, image_url) {
 classify <- function(images, ...) {
   do.call('rbind', pblapply(images, function(i) {
     x <- classify_one(image_url = i, ...)
-    #print(x)
     return(x)
   }))
 }
